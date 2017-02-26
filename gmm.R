59cecaa8ea36b732846974d142f7a94af2565230
Rob Zinkov <rob@zinkov.com>
2017-02-26 18:09:03 -0500

    Adding jags models

---
+++ gmm.R
@@ -0,0 +1,17 @@
+library('rjags')
+
+N <- 1200
+y <- c(rnorm(400, -7), rnorm(400, 3), rnorm(400, 10))
+ 
+jags <- jags.model('gmm.jags',
+                   data = list('y' = y,
+                               'ones' = rep(1,3),
+                               'N' = N),
+                   n.chains = 1,
+                   n.adapt = 10)
+ 
+update(jags, 1000)
+ 
+jags.samples(jags,
+             c('z', 'phi'),
+             1000)
