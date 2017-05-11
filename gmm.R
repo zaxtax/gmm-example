179584f22006e4899f1214258c28db473fe3a0fe
Rob Zinkov <rob@zinkov.com>
2017-05-11 15:41:10 -0400

    Update Rscript for experimenting

--- gmm.R
+++ gmm.R
@@ -1,16 +1,23 @@
-library('rjags')
-library('coda')
+suppressMessages(library('rjags'))
+suppressMessages(library('coda'))
 
-N <- 12000
-y <- c(rnorm(4000, -7), rnorm(4000, 3), rnorm(4000, 10))
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  N <- 1200
+  y <- c(rnorm(400, -7), rnorm(400, 3), rnorm(400, 10))
+} else {
+  N <- as.numeric(args[1])
+  y <- c(rnorm(N/3, -7), rnorm(N/3, 3), rnorm(N/3, 10))
+}
 
- 
+
 jags <- jags.model('gmm.jags',
                    data = list('y' = y,
                                'ones' = rep(1,3),
-                               'N' = N),
+                               'N' = 3*floor(N/3)),
                    n.chains = 1,
-                   n.adapt = 10)
+                   n.adapt = 10,
+                   quiet=TRUE)
 
 start.time <- Sys.time()
  
@@ -24,4 +31,6 @@ samples <- jags.samples(jags,
                         1);
 
 end.time <- Sys.time()
-end.time - start.time
+duration <- end.time - start.time
+# format(duration)
+cat("JAGS",N,as.double(duration), sep=",", fill=TRUE)
